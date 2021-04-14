ee9cb7396c7d1c2c7fd331e5890173d965a18213
GP <gregpetropoulos@yahoo.com>
2021-04-14 05:48:40 -0400

    Saved schema and ran in MySQL to verify, started working on index.js and the prompts are finished and run in the command line.

--- index.js
+++ index.js
@@ -5,3 +5,49 @@
 //  npm install inquirer
 // npm install console.table --save
 // npm install mysql
+
+const mysql = require('mysql');
+const init = require('inquirer');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    // My Port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: 'root',
+
+    // MySQL password
+    password:'password',
+    database:'employeeDB',   
+});
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    badCompany();
+  });
+  
+badCompany = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+              'Add a department',
+              'Add an employee',
+              'Add a role',
+              'View a department',
+              'View an employee',
+              'View a role',
+              'Update employee roles',
+              'Update employee managers',
+              'View employees by manager',
+              'Delete departments, roles, and employees',
+              'View the total utilized budget of a department'
+            ],
+    })
+    }
