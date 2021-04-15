1a895837beebe06fd59378255e6b1c3585a443b1
GP <gregpetropoulos@yahoo.com>
2021-04-15 06:15:36 -0400

    Assigned a switch case it's individual function, working on the addDepartment function to INSERT a row

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
 //  npm install inquirer
 // npm install console.table --save
 // npm install mysql
-
+const conslTable = require('console.table');
+const chalk = require('chalk');
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const connection = require('./config/connection')
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -45,40 +47,68 @@ badCompany = () => {
               'Update employee roles',
               'Update employee managers',
               'View employees by manager',
-              'Delete departments, roles, and employees',
+              'Delete departments', 
+              'Delete roles',
+              'Delete employees',
               'View the total utilized budget of a department'
             ],
     })
     .then ((answer) => {
         switch(answers.action) {
             case 'Add a department':
+                addDepartment();
+                break;
+
             case 'Add an employee':
+                addEmployee();
+                break;
+
             case 'Add a role':
-                addToCompany(); 
+                addRole(); 
                 break;  
-            
+
             case 'View a department':
+                viewDepartment();
+                break;
+
             case 'View an employee':
+                viewEmployee();
+                break;
+
             case 'View a role':
+                viewRole();
+                break;
+
                 // Bonus case
                 case 'View employees by manager':
-                viewCompany();
+                viewEmpByManager();
                 break;
 
             case 'Update employee roles':
+                updateEmpRoles();
+                break;
+
                 // Bonus case
                  case 'Update employee managers':
-                updateCompany();
+                updateEmpManagers();
                 break;
 
                 //  Bonus delete function
-                case 'Delete departments, roles, and employees':
-                deleteFromCompany();
+                case 'Delete departments':
+                deleteDepartments();
+                break;
+
+                case 'Delete roles':
+                deleteRoles();
+                break;
+
+                case 'Delete employees':
+                deleteEmployees();
                 break;
 
                 // Bonus
                 case 'View the total utilized budget of a department':
-                    budgetOfCompany();
+                    companyBudget();
                     break;
 
                     default:
@@ -90,11 +120,45 @@ badCompany = () => {
 
 // fx outside of switch case to handle the prompts
 
-const addToCompany = () => {};
-const viewCompany = () => {};
-const updateCompany = () => {};
-const deleteFromCompany = () => {};
-const budgetOfCompany = () => {};
+const addDepartment = () => {
+    // show the current deleteDepartments
+    query = `SELECT name AS Departments FROM departments`;
+    connection.query(query, (err,results) => {
+        if(err) throw err;
+        console.log('');
+        console.table(chalk.blue('List of the current Departments'), results);
+        
+    })
+    inquirer
+        .prompt({
+            name: 'newDept',
+            type: 'input',
+            Message: 'What department would you like to add?',
+        })
+        .then((answer) => {
+            const query ="INSERT INTO department(name) VALUES('?')";
+            connection.query(query,{department:answer.department}, (err,res) => {
+                res.forEach(({name}) => {
+                    console.log(`Name: ${name}`);   
+                });
+            badCompany();
+            });
+        });
+};
+const addEmployee = () => {};
+const addRole = () => {};
+const viewDepartment = () => {};
+const viewEmployee = () => {};
+const viewRole = () => {};
+const viewEmpByManager = () => {};
+const updateEmpRoles = () => {};
+const updateEmpManagers = () => {};
+const deleteDepartments = () => {};
+const deleteRoles = () => {};
+const deleteEmployees = () => {};
+const companyBudget = () => {};
+
+
 
 
 
